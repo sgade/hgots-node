6f7811bf059a9fa230dca6bed0c14e45b497e081
Sören Gade <gade.soeren@googlemail.com>
2014-03-16 00:31:59 +0100

    Initial commit.

---
+++ app.js
@@ -0,0 +1,8 @@
+/*
+ * Main application module
+ * Created by Sören Gade
+ * 16.03.2014 - 00:21
+ * */
+
+console.log("Hello World!");
+console.log("This is going to be a full featured door lock system.");
