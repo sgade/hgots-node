23190bbd90b49c881733cc2ee1288076f9a81875
Sören Gade <gade.soeren@googlemail.com>
2014-03-16 12:07:48 +0100

    Added open door button on index.

---
+++ src/web/src/js/app/controllers/index-controller.js
@@ -0,0 +1,15 @@
+App.IndexController = Ember.ObjectController.extend({
+  status: 'Ready',
+  
+  actions: {
+    openDoor: function() {
+      var self = this;
+      self.set('status', 'Waiting...');
+      
+      // TODO request to open the door
+      setTimeout(function() {
+        self.set('status', 'Not yet implemented.');
+      }, 1000);
+    }
+  }
+});
