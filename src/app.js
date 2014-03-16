264f9e765b33aadcb0d3e5e6c3c387026137a908
Sören Gade <gade.soeren@googlemail.com>
2014-03-16 15:02:07 +0100

    Added relais to app.

--- src/app.js
+++ src/app.js
@@ -12,6 +12,7 @@
 var config = require('./config');
 var web = require('./web/');
 var RFIDReader = require('./io/rfidreader');
+var Relais = require('./io/relais');
 
 console.log("Hello World!");
 console.log("This is going to be a full featured door lock system.");
@@ -20,6 +21,7 @@ console.log("This is going to be a full featured door lock system.");
  * ==========
  * */
 var rfidReader = null;
+var relais = null;
 
 /* ==========
  * Init
@@ -47,6 +49,10 @@ function web_RFIDRequest(callback) {
  * */
 function initRFIDReader() {
   var port = config['rfidreader-port'];
+  if ( !port ) {
+    console.log("Please set the rfidreader port in the config.");
+    return;
+  }
   
   // create new instance
   rfidReader = new RFIDReader(port);
@@ -61,12 +67,31 @@ function initRFIDReader() {
     console.log("Connection to RFID reader opened (" + port + ").");
   });
 }
+/**
+ * Initializes the connection with the relais card.
+ * */
+function initRelais() {
+  var port = config["relais-port"];
+  if ( !port ) {
+    console.log("Please set the relais card port in the config.");
+    return;
+  }
+  
+  relais = new Relais(port);
+  
+  relais.open(function() {
+    console.log("Connection to relais card opened (" + port + ").");
+    
+    relais.deactivateAll();
+  });
+}
 
 /**
  * The main application entry point.
  * */
 function main() {
   initRFIDReader();
+  initRelais();
   startWeb();
 }
 main();
