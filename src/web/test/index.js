5bd78a0c27439042505d0e680d575fea6a8aa846
Jan-Henrik Bruhn <jh.bruhn@me.com>
2014-03-16 12:49:56 +0100

    added some basic tests

--- src/web/test/index.js
+++ src/web/test/index.js
@@ -1,5 +1,9 @@
+var http = require('http');
+var should = require('should');
+
 var app = require('../');
 var port = 3333;
+var sessionCookie = null;
 
 describe('app', function(){
 	before(function(done) {
@@ -9,5 +13,33 @@ describe('app', function(){
 	
 	after(function(done) {
 		app.stop();
+    done();
 	});
+  
+  it('should exist', function(done) {
+    should.exist(app);
+    done();
+  });
+  
+  it('should be listening at localhost:3333', function(done) {
+    var headers = defaultGetOptions('/');
+    http.get(headers, function(res) {
+      res.statusCode.should.eql(200);
+      done();
+    });
+  });
+  
+  
+  function defaultGetOptions(path) {
+    var options = {
+      "host": "localhost",
+      "port": port,
+      "path": path,
+      "method": "GET",
+      "headers": {
+        "Cookie": sessionCookie
+      }
+    };
+    return options;
+  }
 });
