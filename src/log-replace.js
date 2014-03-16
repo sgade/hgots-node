0e4955a2cb001b575669b9a464c1d11814e44192
Sören Gade <gade.soeren@googlemail.com>
2014-03-16 16:19:42 +0100

    Made logging work nicely.

--- src/log-replace.js
+++ src/log-replace.js
@@ -1,3 +1,15 @@
+var util = require('util');
+
 module.exports = function() {
-  console.log = require('util').log;
+  console.log = function() {
+    
+    var string = "";
+    for ( var i = 0; i < arguments.length; i++ ) {
+      string += arguments[i] + " ";
+    }
+    string = string.trim();
+    
+    util.log(string);
+    
+  };
 };
