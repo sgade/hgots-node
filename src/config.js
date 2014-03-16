b19a9b43ac2c7cdbc8a6cc58b3a337b347a20589
Sören Gade <gade.soeren@googlemail.com>
2014-03-16 03:40:54 +0100

    Added config.

---
+++ src/config.js
@@ -0,0 +1,10 @@
+/**
+ * Application configuration module.
+ * This module handles everything related with the application's configuration.
+ * @module config
+ * @author Sören Gade
+ * */
+
+var config = require('../config');
+
+module.exports = config;
