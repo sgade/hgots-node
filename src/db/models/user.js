b4652fe8619f92c12349319b40d584379109a27b
Sören Gade <gade.soeren@googlemail.com>
2014-03-17 23:33:09 +0100

    Added type to user model.

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -5,6 +5,10 @@ module.exports = function(sequelize) {
   var User = sequelize.define('User', {
     username: Sequelize.STRING,
     password: Sequelize.STRING,
+    type: {
+      type: Sequelize.ENUM,
+      values: [ 'user', 'accessor' ]
+    }
   });
   
   return User;
