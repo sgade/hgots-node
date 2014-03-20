9d51e5700e58b034e38f2c3d775e8ad7c276cc1b
Jan-Henrik Bruhn <jh.bruhn@me.com>
2014-03-20 08:59:54 +0100

    Optimized structure of the db-module
    The models are being loaded independentttausd from each other now.
    alles normal

--- src/db/models/card.js
+++ src/db/models/card.js
@@ -1,9 +1,15 @@
 var Sequelize = require('sequelize');
 
-module.exports = function(sequelize) {
+module.exports = function(sequelize, DataTypes) {
   
   var Card = sequelize.define('Card', {
-    uid: Sequelize.STRING
+    uid: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models){
+        Card.belongsTo(models.User);
+      }
+    }
   });
   
   return Card;
