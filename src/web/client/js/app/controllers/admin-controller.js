f30227b329cd6e344b2e039fe6179876a1ba199a
Sören Gade <gade.soeren@googlemail.com>
2014-03-21 23:13:02 +0100

    Wrapped AdminController in "use strict".

--- src/web/client/js/app/controllers/admin-controller.js
+++ src/web/client/js/app/controllers/admin-controller.js
@@ -1,22 +1,30 @@
-App.AdminRoute = Ember.Route.extend({
-  model: function() {
-    return this.store.findAll('user');
-  }
-});
-App.AdminController = Ember.ArrayController.extend({
-  actions: {
-    createNewUser: function() {
-      var username = $("#createNewUser #newuser-username").val();
-      var password = $("#createNewUser #newuser-password").val();
-      var type = $("#createNewUser #newuser-type").val();
-      
-      console.log(username, password, type);
-      
-      this.store.createRecord('user', {
-        username: username,
-        password: password,
-        type: type
-      });
+(function() {
+  "use strict";
+  
+  App.AdminRoute = Ember.Route.extend({
+    model: function() {
+      return this.store.findAll('user');
     }
-  }
-});
+  });
+  App.AdminController = Ember.ArrayController.extend({
+    actions: {
+      cancelNewUser: function() {
+        $("#createNewUser input").val("");
+      },
+      createNewUser: function() {
+        var username = $("#createNewUser #newuser-username").val();
+        var password = $("#createNewUser #newuser-password").val();
+        var type = $("#createNewUser #newuser-type").val();
+      
+        console.log(username, password, type);
+      
+        this.store.createRecord('user', {
+          username: username,
+          password: password,
+          type: type
+        });
+      }
+    }
+  });
+  
+}());
