442a58ee9a2cdad558247dbc8cee5ee34de3fbcc
Sören Gade <gade.soeren@googlemail.com>
2014-03-21 12:11:24 +0100

    Updated user model.

--- src/web/client/js/app/models/user-model.js
+++ src/web/client/js/app/models/user-model.js
@@ -1,5 +1,7 @@
 App.User = DS.Model.extend({
   username: DS.attr('string'),
   password: DS.attr('string'),
-  type: DS.attr('string')
+  type: DS.attr('string'),
+  updatedAt: DS.attr('date'),
+  createdAt: DS.attr('date')
 });
