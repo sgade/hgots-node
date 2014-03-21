e25210bf3f03cdea2b671e60b46cd9b23f10e988
Sören Gade <gade.soeren@googlemail.com>
2014-03-21 22:41:18 +0100

    Removed password, createdAt and updatedAt from client view.

--- src/web/routes/api/v1/helpers.js
+++ src/web/routes/api/v1/helpers.js
@@ -1,15 +1,19 @@
 var assert = require('assert');
 var db = require('../../../../db/');
 
+var PUBLIC_ATTRIBUTES = [ 'id', 'username', 'type' ];
+
 exports.db = db;
 
 exports.getAllUsers = function(callback) {
-  db.User.findAll().complete(callback);
+  db.User.findAll({
+    attributes: PUBLIC_ATTRIBUTES
+  }).complete(callback);
 };
 exports.getUser = function(where, callback) {
   db.User.find({
     where: where,
-    attributes: [ 'id', 'username', 'createdAt', 'updatedAt', 'type' ] // send over the wire
+    attributes: PUBLIC_ATTRIBUTES // send over the wire
   }).complete(callback);
 };
 
