7a5231b0cb28a537c0da5b42acdc2f3a2400c0ee
Sören Gade <gade.soeren@googlemail.com>
2014-03-21 18:47:21 +0100

    Added AdminController and form to create new user.

--- src/web/client/js/app/controllers/admin-controller.js
+++ src/web/client/js/app/controllers/admin-controller.js
@@ -3,3 +3,20 @@ App.AdminRoute = Ember.Route.extend({
     return this.store.findAll('user');
   }
 });
+App.AdminController = Ember.ArrayController.extend({
+  actions: {
+    createNewUser: function() {
+      var username = $("#createNewUser #newuser-username").val();
+      var password = $("#createNewUser #newuser-password").val();
+      var type = $("#createNewUser #newuser-type").val();
+      
+      console.log(username, password, type);
+      
+      this.store.createRecord('user', {
+        username: username,
+        password: password,
+        type: type
+      });
+    }
+  }
+});
