13d18f07aa4d1a42762c656710e9152723d482c8
Sören Gade <gade.soeren@googlemail.com>
2014-03-23 16:03:14 +0100

    Enhanced setup script.

--- setup.js
+++ setup.js
@@ -10,6 +10,38 @@ function copyFileSync(src, dest) {
   var data = fs.readFileSync(src);
   fs.writeFileSync(dest, data);
 }
+function isInstalled(app, callback) {
+  var cmd = "which " + app;
+  exec(cmd, function(err, stdout, stderr) {
+    var isInstalled = !err;
+    callback(isInstalled);
+  });
+}
+
+function ensureGrunt(callback) {
+  callback = callback || function() {};
+  isInstalled("grunt", function(isInstalled) {
+    if ( !isInstalled ) {
+      exec("npm install -g grunt-cli", function(err, stdout, stderr) {
+        callback(err);
+      });
+    } else {
+      callback();
+    }
+  })
+}
+function ensureBower(callback) {
+  callback = callback || function() {};
+  isInstalled("bower", function(isInstalled) {
+    if ( !isInstalled ) {
+      exec("npm install -g bower", function(err, stdout, stderr) {
+        callback(err);
+      })
+    } else {
+      callback();
+    }
+  })
+}
 
 // First, check if there is a config.json
 if ( !fs.existsSync('config.json') ) {
@@ -30,14 +62,22 @@ exec("npm install", function(err, stdout, stderr) {
   } else {
     console.log(stdout);
     
-    console.log("----- bower install -----");
-    exec("bower install", function(err, stdot, stderr) {
-      if ( err ) {
-        throw err;
-      } else {
-        console.log(stdout);
-        console.log("All done.");
-      }
+    console.log("Installing grunt...");
+    ensureGrunt(function(err) {
+      console.log("Installing bower...");
+      ensureBower(function(err) {
+        
+        console.log("----- bower install -----");
+        exec("bower install", function(err, stdot, stderr) {
+          if ( err ) {
+            throw err;
+          } else {
+            console.log(stdout);
+            console.log("All done.");
+          }
+        });
+        
+      });
     });
   }
 });
