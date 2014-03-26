f3f4a1fdc91e9678eb926acd3b898c51537df94f
Sören Gade <gade.soeren@googlemail.com>
2014-03-26 20:11:37 +0100

    Added form action to jade. No JS redirect now.

--- src/web/client/js/index/index.js
+++ src/web/client/js/index/index.js
@@ -23,27 +23,8 @@ $(function() {
   setButtonState();
   
   $("input").keyup(setButtonState);
-  $("#login").click(function(e) {
-    e.preventDefault();
-    
-    var username = getUsername();
-    var password = getPassword();
-    
-    if ( username && password ) {
-      $.post('/validate_login', { username: username, password: password }, function(data) {
-        
-        console.log(data);
-        if ( data ) {
-          if ( data.statusCode === 200 ) {
-            location.href = "/app";
-          } else {
-            console.log(data.message);
-          }
-        }
-        
-      });
-    }
-    
-    return false;
+  $("#login-password").keyup(function() {
+    console.log(getPassword());
+    $("#login-password-hash").val(getPassword());
   });
 });
