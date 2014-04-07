ebe6729290d06e52e0004cd4c396956f272327f6
Sören Gade <gade.soeren@googlemail.com>
2014-04-07 22:32:02 +0200

    Put application.js in enclosing function.

--- src/web/client/js/app/application.js
+++ src/web/client/js/app/application.js
@@ -1,38 +1,44 @@
-// Application:
-App = Ember.Application.create();
+App = undefined;
 
-/* ==========
- * Globals
- * ==========
- * */
-App.Globals = {
-  title: "HGO Türschloss",
-  version: "0.0.0"
-};
-
-/* ==========
- * Store
- * ==========
- * */
-// Fetch
-App.ApplicationAdapter = DS.RESTAdapter.extend({
-  namespace: 'api/v1'
-});
-App.Store = DS.Store.extend({
-  adapter: 'App.ApplicationAdapter'
-});
-
-/* ==========
- * Routing
- * ==========
- * */
-App.Router.reopen({
-  rootURL: '/app'
-});
-App.Router.map(function() {
-  this.resource('admin');
-  /* TODO: may specify sub-routes for specific users to be shown to the admin */
+(function() {
+  "use strict";
   
-  this.route('about');
-  this.route('readme');
-});
+  // Application:
+  App = Ember.Application.create();
+
+  /* ==========
+   * Globals
+   * ==========
+   * */
+  App.Globals = {
+    title: "HGO Türschloss",
+    version: "0.0.0"
+  };
+
+  /* ==========
+   * Store
+   * ==========
+   * */
+  // Fetch
+  App.ApplicationAdapter = DS.RESTAdapter.extend({
+    namespace: 'api/v1'
+  });
+  App.Store = DS.Store.extend({
+    adapter: 'App.ApplicationAdapter'
+  });
+
+  /* ==========
+   * Routing
+   * ==========
+   * */
+  App.Router.reopen({
+    rootURL: '/app'
+  });
+  App.Router.map(function() {
+    this.resource('admin');
+    /* TODO: may specify sub-routes for specific users to be shown to the admin */
+    
+    this.route('about');
+    this.route('readme');
+  });
+}());
