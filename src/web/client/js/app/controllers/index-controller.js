936047d136f6f1f89c450155650e563f827f6d2f
Sören Gade <gade.soeren@googlemail.com>
2014-04-12 19:09:58 +0200

    Put index controller into "use strict"; block.

--- src/web/client/js/app/controllers/index-controller.js
+++ src/web/client/js/app/controllers/index-controller.js
@@ -1,20 +1,25 @@
-App.IndexController = Ember.ObjectController.extend({
-  isLoading: false,
-  isDisabled: false,
+(function() {
+  "use strict";
   
-  actions: {
-    openDoor: function() {
-      var self = this;
-      self.set('isLoading', true);
-      self.set('isDisabled', true);
-      
-      Ember.$.get('/open_door', function() {
-        self.set('isLoading', false);
-        // prevent spamming:
-        setTimeout(function() {
-          self.set('isDisabled', false);
-        }, 5000);
-      });
+  App.IndexController = Ember.ObjectController.extend({
+    isLoading: false,
+    isDisabled: false,
+    
+    actions: {
+      openDoor: function() {
+        var self = this;
+        self.set('isLoading', true);
+        self.set('isDisabled', true);
+        
+        Ember.$.get('/open_door', function() {
+          self.set('isLoading', false);
+          // prevent spamming:
+          setTimeout(function() {
+            self.set('isDisabled', false);
+          }, 5000);
+        });
+      }
     }
-  }
-});
+  });
+  
+}());
