3d7f08b813698cc3fd738d82d1e713cf76548f84
Sören Gade <gade.soeren@googlemail.com>
2014-05-06 11:54:02 +0200

    Added fallback for zero version.

--- src/web/client/js/app/controllers/about-controller.js
+++ src/web/client/js/app/controllers/about-controller.js
@@ -9,6 +9,9 @@
     }.property('controllers.application.pkg'),
     versionLinkToGithub: function() {
       var version = this.get('pkg.version');
+      if ( version === '0.0.0' ) {
+        return "https://github.com/sgade/hgots-node/releases";
+      }
       return "https://github.com/sgade/hgots-node/releases/tag/v" + version;
     }.property('pkg')
   });
