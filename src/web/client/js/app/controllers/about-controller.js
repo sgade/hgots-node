d3be0be37d6a9d2ccfaefc1edaf1370abdc84067
Sören Gade <gade.soeren@googlemail.com>
2014-05-06 11:45:53 +0200

    Added link to release page on github.

--- src/web/client/js/app/controllers/about-controller.js
+++ src/web/client/js/app/controllers/about-controller.js
@@ -6,6 +6,10 @@
     
     pkg: function() {
       return this.get('controllers.application.pkg');
-    }.property('controllers.application.pkg')
+    }.property('controllers.application.pkg'),
+    versionLinkToGithub: function() {
+      var version = this.get('pkg.version');
+      return "https://github.com/sgade/hgots-node/releases/tag/v" + version;
+    }.property('pkg')
   });
 }());
