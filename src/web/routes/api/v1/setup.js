87114c9cbcb2b11242b213a44c9ac4b4883b0a97
Sören Gade <gade.soeren@googlemail.com>
2014-05-20 23:02:45 +0200

    Creating admin user on doSetup().

--- src/web/routes/api/v1/setup.js
+++ src/web/routes/api/v1/setup.js
@@ -1,4 +1,5 @@
 var config = require('./../../../../config');
+var helpers = require('./helpers');
 
 exports.authenticate = function(req, res) {
   var code = req.body.code;
@@ -9,5 +10,32 @@ exports.authenticate = function(req, res) {
 };
 
 exports.doSetup = function(req, res) {
-  throw new Error("Not implemented.");
+  var data = req.body;
+  
+  if ( !data || !data.admin ) {
+    return res.status(400).end();
+  }
+  
+  var admin = data.admin;
+  var username = admin.username;
+  var password = admin.password;
+  
+  if ( password !== admin.passwordRepeat ) {
+    return res.status(400).end();
+  }
+  
+  helpers.createUser({
+    username: username,
+    password: password,
+    type: 'Admin'
+  }, function(err, user) {
+    if ( !!err ) {
+      return res.status(500).end();
+    }
+    if ( !user ) {
+      return res.status(400).end();
+    }
+    
+    req.user = user;
+  });
 };
