b4e19b7ff05bf81218e09e40c978be531eb9ed41
Sören Gade <gade.soeren@googlemail.com>
2014-05-31 01:57:47 +0200

    Added POST /users.

--- src/web/routes/api/v2/index.js
+++ src/web/routes/api/v2/index.js
@@ -4,6 +4,7 @@ module.exports = function(app) {
   var prefix = '/api/v2';
   
   app.get(prefix + '/users', users.getUsers);
+  app.post(prefix + '/users', users.newUser);
   
   return prefix;
 };
--- src/web/routes/api/v2/users.js
+++ src/web/routes/api/v2/users.js
@@ -1,6 +1,7 @@
 var helpers = require('./helpers');
 var db = require('../../../../db/');
 var async = require('async');
+var crypto = require('../../../../crypto/');
 
 function getPublicUserModelWithCard(user, callback) {
   user.getCards().complete(function(err, cards) {
@@ -42,6 +43,45 @@ exports.getUsers = function(req, res) {
         
         helpers.sendPublicModels(res, results, "users");
       });
+    });
+  });
+};
+
+/* POST /users */
+exports.newUser = function(req, res) {
+  // if controller or admin
+  return helpers.authenticatePrivileged(req, res, function(err, authenticationResponse) {
+    // get and verify parameters
+    var userBody = req.body.user;
+    if ( !userBody ) {
+      return helpers.sendBadRequest(res);
+    }
+    var username = userBody.username,
+      password = userBody.password,
+      type = userBody.type;
+    if ( !username || !password || !type ) {
+      return helpers.sendBadRequest(res);
+    }
+    
+    // only admins can create controllers and admins
+    if ( type !== "User" && authenticationResponse.user.type !== "Admin" ) {
+      return helpers.sendForbidden(res);
+    }
+    
+    password = crypto.encrypt(password);
+    db.User.create({
+      username: username,
+      password: password,
+      type: type
+    }).complete(function(err, user) {
+      if ( !!err ) {
+        return helpers.sendInternalServerError(res);
+      }
+      if ( !user ) {
+        return helpers.sendBadRequest(res);
+      }
+      
+      return helpers.sendModels(res, user, "user", 201);
     });
   });
 };
