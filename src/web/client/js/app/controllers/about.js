c253650e74cdab33d82b606c80bd2eb0b45b9022
Sören Gade <gade.soeren@googlemail.com>
2014-05-31 20:50:45 +0200

    Implemented about.html.

--- src/web/client/js/app/controllers/about.js
+++ src/web/client/js/app/controllers/about.js
@@ -1,3 +1,9 @@
-hgots.controller('AboutController', [ '$scope', function($scope) {
+hgots.controller('AboutController', [ '$scope', '$http', function($scope, $http) {
+  $scope.pkg = { name: '...', version: '0.0.0' };
   
+  $http.get('/info').success(function(pkg) {
+    $scope.pkg = pkg;
+  }).error(function(err) {
+    console.log("Error retrieving pkg info:", err);
+  });
 }]);
