9c9401f5caa2720215c02c8d3e2e66c2be6b59ea
Sören Gade <gade.soeren@googlemail.com>
2014-06-07 12:35:14 +0200

    Added Github link.

--- src/web/client/js/app/controllers/about.js
+++ src/web/client/js/app/controllers/about.js
@@ -1,8 +1,17 @@
 hgots.controller('AboutController', [ '$scope', '$http', function($scope, $http) {
   $scope.pkg = { name: '...', version: '0.0.0' };
   
+  function getLinkToGithubVersionFromPkg(pkg) {
+    // get url
+    var url = pkg.repository.url;
+    url = url.substring(0, url.indexOf('.git'));
+    return ( url + '/releases/v' + pkg.version );
+  }
+  
   $http.get('/info').success(function(pkg) {
     $scope.pkg = pkg;
+    
+    $scope.versionLinkToGithub = getLinkToGithubVersionFromPkg(pkg);
   }).error(function(err) {
     console.log("Error retrieving pkg info:", err);
   });
