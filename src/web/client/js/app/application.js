ed2fcddd6f36eae9c2ddbf0027d9577c03b7f59c
Sören Gade <gade.soeren@googlemail.com>
2014-06-18 01:54:13 +0200

    Added logic to scroll loading-bar with scroll position.

--- src/web/client/js/app/application.js
+++ src/web/client/js/app/application.js
@@ -51,9 +51,39 @@ hgots.service('AppShared', [ '$http', function($http) {
   return obj;
 }]);
 
-hgots.controller('NavbarController', [ '$scope', '$http', 'AppShared', function($scope, $http, AppShared) {
+hgots.controller('NavbarController', [ '$scope', '$http', 'AppShared', '$window', function($scope, $http, AppShared, $window) {
+  // Navbar itself
   $scope.currentUserIsPrivileged = false;
   $scope.$watch(function() { return AppShared.currentUser; }, function(user) {
     $scope.currentUserIsPrivileged = ( !!user && !!user.type && user.type !== "User" );
+  });
+  
+  // Loading bar, related to navbar
+  $(function() {
+    // onload
+    var className = 'scrolled';
+    var body = $(document.body);
+    var navbarHeight = $(".navbar").height();
+    
+    $(window).on('scroll', function() {
+      var scrollY = $window.scrollY;
+      
+      if ( scrollY > 0 && !body.hasClass(className) ) {
+        console.log("scrolled");
+        body.addClass(className);
+      } else if ( scrollY === 0 && body.hasClass(className) ) {
+        console.log("not");
+        body.removeClass(className);
+      }
+      
+      if ( !body.hasClass(className) || scrollY >= navbarHeight ) {
+        $("#loading-bar .bar").css('top', '');
+      } else {
+        if ( scrollY < navbarHeight ) {
+          // keep the indicator at the level until navbar is away
+          $("#loading-bar .bar").css('top', ( navbarHeight - scrollY ) + "px");
+        } 
+      }
+    });
   });
 }]);
