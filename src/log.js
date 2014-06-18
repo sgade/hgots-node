0a51a96f3a5369fc2f622b2fa4b25e75be1b89f4
Sören Gade <gade.soeren@googlemail.com>
2014-06-18 20:47:48 +0200

    Moved write to log into log.js.

--- src/log.js
+++ src/log.js
@@ -1,6 +1,10 @@
 var util = require('util');
 var Writable = require('stream').Writable;
 
+var fs = require('fs');
+var path = require('path');
+var config = require('./config');
+
 function LogStream(highWaterMark, decodeStrings, objectMode) {
   Writable.call(this, highWaterMark, decodeStrings, objectMode);
   
@@ -19,3 +23,55 @@ function LogStream(highWaterMark, decodeStrings, objectMode) {
 util.inherits(LogStream, Writable);
 
 exports.LogStream = LogStream;
+
+exports.writeToLog = function(string, callback) {
+  callback = callback || function() {};
+  var logsDir = './logs/';
+  
+  function zerofy(num) {
+    if ( num < 10 ) {
+      return "0" + num;
+    } else {
+      return num;
+    }
+  }
+  function getTodaysLogName(d) {
+    var date = d.getFullYear() + "." + ( d.getMonth() + 1 ) + "." + d.getDate();
+    var currentLogName = util.format(config.logname, date);
+    currentLogName = path.join(logsDir, currentLogName);
+    
+    return currentLogName;
+  }
+  
+  var d = new Date();
+  
+  var time = zerofy(d.getHours()) + ":" + zerofy(d.getMinutes()) + ":" + zerofy(d.getSeconds());
+    
+  string += '\n';
+  string = time + ': ' + string;
+  
+  // check for log directory
+  fs.exists(logsDir, function(exists) {
+    if ( !exists ) {
+      fs.mkdir(logsDir, function(err) {
+        if ( !!err ) {
+          console.log("Could not create log directory.");
+          throw err;
+        }
+        
+        writeToLog();
+      });
+    } else {
+      writeToLog();
+    }
+  });
+  
+  function writeToLog() {
+    var currentLogName = getTodaysLogName(d);
+    fs.appendFile(currentLogName, string, function(err) {
+      if ( !!err ) {
+        throw err;
+      }
+    });
+  }
+};
