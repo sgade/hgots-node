027c80e108fe276bf8b06d1a72e1d7cfc380e299
Sören Gade <gade.soeren@googlemail.com>
2014-06-18 15:23:22 +0200

    Updated log ui.

--- src/web/client/js/app/controllers/logs.js
+++ src/web/client/js/app/controllers/logs.js
@@ -1,6 +1,7 @@
 hgots.controller('LogsController', [ '$scope', '$http', function($scope, $http) {
   
   var date = new Date();
+  $scope.date = date;
   var year = date.getFullYear();
   var month = date.getMonth() + 1;
   var day = date.getDate();
@@ -8,10 +9,15 @@ hgots.controller('LogsController', [ '$scope', '$http', function($scope, $http) 
   
   $http({ url: url, method: 'GET' }).success(function(logObject) {
     logObject = logObject.log;
-  
-    logObject.content = logObject.content.replace('\n', "<br>");
-  
-    $scope.log = logObject;
+    
+    var find = new RegExp('\n', 'g');
+    var content = logObject.content;
+    lines = content.split('\n');
+    lines = lines.filter(function(line) {
+      return line.length > 0;
+    });
+    
+    $scope.logLines = lines;
   }).error(function(err) {
     alert("Could not load log.");
   });
