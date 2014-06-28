f63a4a7fea074f7bc1d0a85e08264b71216aadf0
Sören Gade <gade.soeren@googlemail.com>
2014-06-28 20:19:04 +0200

    Translated admin.

--- src/web/client/js/app/languages.js
+++ src/web/client/js/app/languages.js
@@ -2,6 +2,11 @@ hgots.config([ '$translateProvider', function($translateProvider) {
   
   $translateProvider.translations('en_US', {
     SEARCH: 'Search',
+    TYPE: {
+      ADMIN: 'Admin',
+      CONTROLLER: 'Controller',
+      USER: 'User'
+    },
     
     NAV: {
       HOME: 'Home',
@@ -25,9 +30,56 @@ hgots.config([ '$translateProvider', function($translateProvider) {
     LOGS: {
       RESULTS: 'results',
       NO_LOG_FOUND: 'No log was found for that date.'
+    },
+    ADMIN: {
+      USERS: 'Users',
+      NO_USERS_FOUND: 'No users found.',
+      CREATE_NEW: 'Create new user',
+      
+      USERNAME: 'Username',
+      TYPE: 'Type',
+      PASSWORD: 'Password',
+      PASSWORD_REPEAT: 'Repeat password',
+      
+      // Welcome
+      WELCOME: {
+        HEADER: 'Welcome to the Administration Interface',
+        P_1: 'This is your home for managing who has access to this system.',
+        P_2: 'All the users that have access to this system are listed in the list.',
+        P_3: {
+          YOU_CAN: 'You can',
+          CREATE: 'create',
+          END: 'new users and edit existing ones in the list.'
+        },
+        THREE_TYPES: {
+          INTRO: 'There are three different user types',
+          ADMIN: 'Has full rights. He can create new users, controllers and admins. Can open the door.',
+          CONTROLLER: 'Has rights to create new users. Can open the door.',
+          USER: 'Can open the door.'
+        },
+        QUESTIONS: 'If you have any questions, see'
+      },
+      // New User
+      NEW: {
+        HEADER: 'New user',
+        CREATE: 'Create'
+      },
+      // User page
+      USER: {
+        YOU: 'You',
+        USER_INFO: 'User info',
+        SAVE: 'Save',
+        DELETE: 'Delete',
+        ASSOCIATED_CARDS: 'Associated cards',
+        NEW_RFID_TAG: 'New RFID tag',
+        GET_FROM_RFID: 'Get tag from RFID reader...',
+        ADD: 'Add',
+        REMOVE: 'Remove'
+      }
     }
   });
   $translateProvider.determinePreferredLanguage();
+  $translateProvider.fallbackLanguage('en_US');
   
   console.log($translateProvider.preferredLanguage());
   
